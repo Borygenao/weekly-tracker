7cafec803821e9f470cd7f13555195c69cf53b9a
Borygenao <87765624+Borygenao@users.noreply.github.com>
2026-03-21 23:10:43 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'weekly-v2';
+const CACHE = 'weekly-v3';
 const ASSETS = ['./', './index.html', './icon-192.png', './icon-512.png', './apple-touch-icon.png', './manifest.json'];
 
 self.addEventListener('install', e => {

5f82c5901e4cf654c2258dc8497709fad4af56c7
Borygenao <87765624+Borygenao@users.noreply.github.com>
2026-03-22 11:00:18 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'weekly-v17';
+const CACHE = 'weekly-v18';
 const ASSETS = ['/weekly-tracker/', '/weekly-tracker/index.html', '/weekly-tracker/icon-192.png', '/weekly-tracker/icon-512.png', '/weekly-tracker/apple-touch-icon.png', '/weekly-tracker/manifest.json'];
 
 self.addEventListener('install', e => {
